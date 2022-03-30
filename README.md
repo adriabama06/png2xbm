fa31679c11973a4c4529ce3f87fa601b26b54dca
adriabama06 <adriabama06@gmail.com>
2022-03-30 20:45:34 +0200

    final version

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-# Install
+# Install:
 
 ## Linux:
 ```bash
 sudo apt install imagemagick
+sudo apt install nodejs
+
+git clone https://github.com/adriabama06/png2xbm.git
+
+cd png2xbm
+
+node png2xbm.js --help
+```
+
+## Windows
+Install [ImageMagick](https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-29-Q16-x64-static.exe)
+Install [NodeJS](https://nodejs.org/dist/v16.14.2/node-v16.14.2-x64.msi)
+```bash
+git clone https://github.com/adriabama06/png2xbm.git
+
+cd png2xbm
+
+node png2xbm.js --help
+```
+
+## Example usage:
+```bash
+node png2xbm.js --input example/ --output out/ --header myheader.h -x 128 -y 53
 ```
--- png2xbm.js
+++ png2xbm.js
@@ -1,28 +1,106 @@
-#!/usr/local/node/bin/node
-
-const resolution_x = 128;
-const resolution_y = 53;
-
-const replace_placehodler = `#define %filename%_width ${resolution_x}
-#define %filename%_height ${resolution_y}
-static char %filename%_bits[] = `;
-
 const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
 
-if(!process.argv[2] || process.argv[2].startsWith('-') && process.argv[2].toLowerCase().replace(/-/g, '').replace('elp', '') == "h") {
-    console.log("./png2xbm.js [INPUT] [OUTPUT]");
-    console.log("No use spaces at input and output");
-    return 0;
-}
-if(!process.argv[3]) {
-    console.log("./png2xbm.js --help");
-    return 0;
+/**
+ * @type {Map<string, string | undefined>}
+ */
+var argvs = new Map();
+
+if(!process.argv[2]) {
+    console.log("node png2xbm.js --help");
+    process.exit(1);
 }
 
-const input_dir = process.argv[2];
-const output_dir = process.argv[3];
+for(var i = 2; i < process.argv.length; i++) {
+    var argv = process.argv[i];
+
+    if(!argv.startsWith("-")) {
+        continue;
+    }
+
+    argv = argv.replace(/-/g, '').toLowerCase();
+    i++;
+
+    var value = process.argv[i];
+    if(value && !argv.startsWith("-") || !argv.startsWith("--")) {
+        argvs.set(argv, value);
+        continue;
+    }
+    i--;
+    argvs.set(argv, undefined);
+}
+
+if(argvs.has("help") || argvs.has("h")) {
+    console.log(`Usage: node png2xbm.js --input FOLDER --output FOLDER
+    Arguments:
+        (Optional) --help, -help, --h, -h : Show this menu
+        
+        (Optional) --header, -header : Output of the header file for C/C++ animation
+        (Optional if --header is not used) --x, -x : Images x resolution
+        (Optional if --header is not used) --y, -y : Images y resolution
+
+        --input, -input, --i, -i : Input folder where have the .png images
+        --output, -output, --o, -o : Output folder where the .xbm file will be saved
+        `);
+    process.exit(0);
+}
+
+if(!argvs.has("input")) {
+    if(argvs.has("i")) {
+        argvs.set("input", argvs.get("i"));
+    }
+}
+
+if(!argvs.has("output")) {
+    if(argvs.has("o")) {
+        argvs.set("output", argvs.get("o"));
+    }
+}
+
+const input_dir = argvs.get("input");
+const output_dir = argvs.get("output");
+const output_header = argvs.get("header");
+
+const resolution_x = parseInt(argvs.get("x"));
+const resolution_y = parseInt(argvs.get("y"));
+
+if(true) {
+    var errorMsg = "";
+
+    if(input_dir == undefined) {
+        errorMsg += "\nInvalid: input";
+    }
+
+    if(output_dir == undefined) {
+        errorMsg += "\nInvalid: output";
+    }
+
+    if(output_header) {
+        if(resolution_x == undefined) {
+            errorMsg += "\nInvalid: if header is enabled -x is required filled";
+        }
+        if(resolution_y == undefined) {
+            errorMsg += "\nInvalid: if header is enabled -y is required filled";
+        }
+
+        if(isNaN(resolution_x)) {
+            errorMsg += "\nInvalid: -x need to be an intiger like: 1, 2, 3";
+        }
+        if(isNaN(resolution_y)) {
+            errorMsg += "\nInvalid: -y need to be an intiger like: 1, 2, 3";
+        }
+    }
+
+    if(errorMsg.length >= 1) {
+        console.log(errorMsg);
+        process.exit(1);
+    }
+}
+
+const replace_placehodler = `#define %filename%_width ${resolution_x}
+#define %filename%_height ${resolution_y}
+static char %filename%_bits[] = `;
 
 /**
  * @type {string[]}
@@ -37,6 +115,11 @@ try {
 }
 
 input = input.filter(file => file.endsWith(".png"));
+
+if(!fs.existsSync(output_dir)) {
+    fs.mkdirSync(output_dir, { recursive: true });
+}
+
 var output = [];
 async function doWork() {
     for(var i = 0; i < input.length; i++) {
@@ -71,6 +154,9 @@ async function doWork() {
         console.log(` - ${time / 1000 / 60 >= 1 ? (time / 1000 / 60) + 'm : ' : ''}${time / 1000}s : ${time}ms`);
     }
 
+    if(!output_header) {
+        return;
+    }
 
     var outtext = `const static char animation[${input.length}][${resolution_x * resolution_y}] = {\n\n`;
 
@@ -87,7 +173,7 @@ async function doWork() {
         outtext += `// frame: ${i}\n${framedata}\n`;
     }
     outtext += "};";
-    fs.writeFileSync(path.join(output_dir, "_outtext.h"), outtext);
+    fs.writeFileSync(output_header, outtext);
 }
 
 doWork()
